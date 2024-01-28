5805cfde7da4d8ac091373accca8567c988a034a
Wildiney Di Masi <wildiney@gmail.com>
2024-01-28 17:14:43 -0300

    convert to pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,38 @@
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg']
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Jokenpo',
+        short_name: 'Jokenpo',
+        description: 'Jokenpo Game',
+        theme_color: '#bc658d',
+        icons: [
+          {
+            src: 'logo192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'logo512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   test: {
     include: ["**/*.test.tsx"],
     globals: true,
