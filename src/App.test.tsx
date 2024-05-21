adf43905f252a71636289354d772fc1ee9b55335
Wildiney Di Masi <wildiney@gmail.com>
2024-05-21 16:02:59 -0300

    update setup testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,6 @@
 // import { render } from '@testing-library/react';
 // import App from './App';
 
-// test('renders learn react link', () => {
-//   const { getByText } = render(<App />);
-//   const linkElement = getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('tests if the test setup is working', () => {
+  expect(true).toBeTruthy();
+});
